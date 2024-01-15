2e2d1678c5c343ab2dc65a32cc2feeed72f132fb
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-01-15 21:17:49 +0300

    rewrite customer function

--- createCustomer.js
+++ createCustomer.js
@@ -2,7 +2,7 @@
 const AWS = require("aws-sdk");
 
 exports.createCustomer = async (event) => {
-    const body = JSON.parse(Buffer.from(event.body, 'base64').toString());
+    const body = JSON.parse(event.body);
     const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
     const putParams = {
